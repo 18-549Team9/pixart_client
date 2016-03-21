349691beed724da4d35d0e13d0d6eb4e77a9a82e
Matthew Du <mdu@andrew.cmu.edu>
2016-03-21 13:59:00 -0400

    document i2c baud rate set to 400 kHz

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# put clk on gpio 4, rst on gpio17
+
+# enable i2c
 cat /etc/modprobe.d/raspi-blacklist.conf |
 grep -v 'blacklist i2c-bcm2708' >
 temp.txt
@@ -12,7 +15,14 @@ temp.txt
 cat 'i2c-dev' >> temp.txt
 mv temp.txt /etc/modules
 
+# set i2c baud rate to fast 400 KHz
+echo 'options i2c_bcm2708 baudrate=400000' > /etc/modprobe.d/i2c.conf
+
+# install i2c tools
 apt-get update
 apt-get install i2c-tools
 adduser pi i2c
+
+# obtain pigpio and start daemon on startup
+
 reboot
