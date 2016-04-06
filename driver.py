19180d7239429d4275e10fd5b732e4acd29be0e0
Matthew Du <mdu@andrew.cmu.edu>
2016-04-06 00:09:06 -0400

    Hardened process against unexpected failure

--- driver.py
+++ driver.py
@@ -13,7 +13,18 @@ STOPPING = 1
 STOPPED = 2
 pi = pigpio.pi()
 
+# Configures GPIO pins to work with the camera
 def initDevice():
+  # Set pin 4 to a 25 MHz clock
+  pi.hardware_clock(4, 20000000)
+
+  # Set pin 17 to 0 to reset the camera as an active-low reset pin
+  pi.set_mode(17, pigpio.OUTPUT)
+  pi.write(17, 0)
+  time.sleep(0.1)
+  pi.write(17, 1)
+
+  # Write startup bytes to the pi
   h = pi.i2c_open(1, DEV_ADDR)
   pi.i2c_write_device(h, [0x30, 0x01])
   time.sleep(0.1)
@@ -29,16 +40,22 @@ def initDevice():
   time.sleep(0.1)
   return h
 
-def sampleToBuffer(outbytes):
+# Samples data from input device to the buffer
+def sampleToBuffer(h, outbytes):
   while True:
-    pi.i2c_write_device(h, [0x37])
-    time.sleep(25/1000000)
-    (count, data1) = pi.i2c_read_device(h, 8)
-    time.sleep(380/1000000)
-    (count, data2) = pi.i2c_read_device(h, 4)
-    outbytes[:] = data1 + data2;
+    try:
+      h = initDevice()
+      while True:
+        pi.i2c_write_device(h, [0x37])
+        time.sleep(25/1000000)
+        (count, data1) = pi.i2c_read_device(h, 8)
+        time.sleep(380/1000000)
+        (count, data2) = pi.i2c_read_device(h, 4)
+        outbytes[:] = data1 + data2;
+    except:
+      pass
 
-# x, y, size
+# Parses a 3 byte blob into a tuple of x, y, and size
 def parseBlob(b):
   if (b[0] == 0xff and b[1] == 0xff and b[2] == 0xff):
     return None
@@ -47,83 +64,93 @@ def parseBlob(b):
   s = b[2] & 0x0F
   return (x, y, s)
 
+# Streams data from the buffer to the input address
 def streamFromBuffer(inbytes, address, streamState):
-  client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  client.bind(('', 56050))
+  try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.bind(('', 0))
 
-  i = 0;
-  while streamState.value == STREAMING:
-    data = inbytes[:]
-    s = (parseBlob(data[0:3]), parseBlob(data[3:6]), parseBlob(data[6:9]), parseBlob(data[9:12]))
-    client.sendto(str((i, s)) + '\n', address) 
-    print str((i, s))
-    i += 1;
-    time.sleep(0.01)
+    i = 0;
+    while streamState.value == STREAMING:
+      data = inbytes[:]
+      s = (parseBlob(data[0:3]), parseBlob(data[3:6]), parseBlob(data[6:9]), parseBlob(data[9:12]))
+      client.sendto(str((i, s)) + '\n', address)
+      print str((i, s))
+      i += 1;
+      time.sleep(0.01)
+  except:
+    pass
+
+  client.close()
   streamState.value = STOPPED;
 
-# Set pin 4 to a 25 MHz clock
-pi.hardware_clock(4, 20000000)
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_GET(self):
+    if self.path == '/status':
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      statuses = ['STREAMING', 'STOPPING', 'STOPPED']
+      self.wfile.write('status is ' + statuses[streamState.value] + '\n')
+    else:
+      self.send_response(404)
+      self.end_headers()
 
-# Set pin 17 to 1 to disable the active-low reset pin
-pi.set_mode(17, pigpio.OUTPUT)
-pi.write(17, 1)
-
-# Initialize i2c camera
-h = initDevice()
-
-# Begin sampling data from the camera
-bytes = multiprocessing.Array('i', range(12))
-streamState = multiprocessing.Value('i', STOPPED);
-p = multiprocessing.Process(target=sampleToBuffer, args=(bytes,))
-p.start()
-
-try:
-  # Wait for an incoming http connection
-  class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(self):
-      if self.path == '/status':
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        statuses = ['STREAMING', 'STOPPING', 'STOPPED']
-        self.wfile.write('status is ' + statuses[streamState.value])
-      else:
-        self.send_response(404)
-        self.end_headers()
-
-    def do_POST(self):
+  def do_POST(self):
+    try:
       length = int(self.headers.getheader('content-length'))
       field_data = self.rfile.read(length)
       fields = urlparse.parse_qs(field_data)
+    except:
+      self.send_response(400) # bad request
+      return
 
-      if self.path == '/start':
-        if 'ip' in fields and 'port' in fields:
-          self.startStreaming((fields['ip'], fields['port']))
-          self.send_response(200)
-          return
-      elif self.path == '/stop':
-        self.stopStreaming()
-        self.send_response(200)
-        return
+    success = False;
+    if self.path == '/start':
+      success = self.startStreaming(fields)
+    elif self.path == '/stop':
+      success = True
+      self.stopStreaming()
+    else:
+      self.send_response(404) # not found
 
-      self.send_response(404)
+    if success:
+      self.send_response(200) # ok
+    else:
+      self.send_response(500) # server error
 
-    def startStreaming(self, host):
-      address = (host[0][0], int(host[1][0]))
+  # Start streaming by spawning a process to stream data
+  def startStreaming(self, fields):
+    try:
+      address = (fields['ip'][0], int(fields['port'][0]))
       if streamState.value == STOPPED:
         streamState.value = STREAMING
         q = multiprocessing.Process(target=streamFromBuffer, args=(bytes,address,streamState))
         q.start()
+        return True
+    except:
+      pass
+    return False
 
-    def stopStreaming(self):
-      if (streamState.value == STREAMING):
-        streamState.value = STOPPING;
+  def stopStreaming(self):
+    if (streamState.value == STREAMING):
+      streamState.value = STOPPING;
 
-  server = BaseHTTPServer.HTTPServer(('', 80), Handler)
-  server.serve_forever()
-except KeyboardInterrupt:
-  server.socket.close()
+def main():
 
-def test():
+  # Begin sampling data from the camera to these bytes
+  bytes = multiprocessing.Array('i', range(12))
+  streamState = multiprocessing.Value('i', STOPPED);
+  p = multiprocessing.Process(target=sampleToBuffer, args=(h, bytes))
+  p.start()
+
+# Wait for an incoming http connection
   while True:
-    pass
+    try:
+      server = BaseHTTPServer.HTTPServer(('', 12345), Handler)
+      server.serve_forever()
+    except:
+      server.socket.close()
+
+if __name__ == '__main__':
+  main()
